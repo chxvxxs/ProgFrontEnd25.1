7dc694e7c9be44bf2de5e8258025f831e001a5b8
Matheus Chaves <88739142+chxvxxs@users.noreply.github.com>
2025-02-14 00:38:25 +0000

    criando uma função que exibe outro texto

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -1,9 +1,13 @@
+function Texto1(){
+  return <p>Um outro parágrafo de texto!</p>;
+}
 export default function Home() {
   const aula = 2;
   return (
     <div>
       <h1>Olá Mundo!</h1>
       <p>Essa é a nossa {aula}ª aula de react</p>
+      <Texto1 />
     </div>
   );
 }
